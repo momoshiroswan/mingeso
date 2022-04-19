57d584744516e44a4f8cd8dea414a2de47dfc9e3
Mónica López <monica.lopez.n@usach.cl>
2022-04-18 21:08:39 -0400

    ingreso de un mensaje en la pagina

---
+++ demo/demo/src/main/java/com/example/demo/MensajeController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MensajeController {
+	@GetMapping("mensaje")
+	public String Mensaje(){
+		return("Aplicación web muy simple");
+	}
+}
